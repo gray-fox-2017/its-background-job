9c6651a1f9f1cec98ee1cc0893ed3d2af175e941
ingelieur <ingelieur@gmail.com>
2017-06-10 20:46:47 +0700

    love, Yoda says

--- controllers/app.js
+++
@@ -1,9 +0,0 @@
-const unirest = require('unirest');
-const uriencoder = require('helpers/uriencoder');
-
-unirest.get("https://yoda.p.mashape.com/yoda?sentence=You+will+learn+how+to+speak+like+me+someday.++Oh+wait.")
-  .header("X-Mashape-Key", "hqcLNYymK8mshr1WONn16eouffPTp1F4mwqjsnMhJEc0RFUA3W")
-  .header("Accept", "text/plain")
-  .end(function (result) {
-      console.log(result.status, result.headers, result.body);
-  });
--- controllers/yoda.js
+++ controllers/yoda.js
@@ -22,17 +22,17 @@ var sayLove = ((req,res) => {
 
 var getQuote = ((callback) => {
   // IT ONLY GETS A QUOTE OF THE DAY. MAX 10 request per hour - in case of emergency, use the bottom one.
-  //unirest.get("http://quotes.rest/qod.json?category=love")
-  //  .header("X-Mashape-Key", "hqcLNYymK8mshr1WONn16eouffPTp1F4mwqjsnMhJEc0RFUA3W")
-  //  .header("Accept", "application/json")
-  //  .end(function (result) {
-  //    callback(result.body.contents.quotes[0].quote);
-  //  });
-  unirest.get("http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=xml&lang=en")
+  unirest.get("http://quotes.rest/qod.json?category=love")
+    .header("X-Mashape-Key", "hqcLNYymK8mshr1WONn16eouffPTp1F4mwqjsnMhJEc0RFUA3W")
     .header("Accept", "application/json")
     .end(function (result) {
-      callback(result.body);
+      callback(result.body.contents.quotes[0].quote);
     });
+  //unirest.get("http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=xml&lang=en")
+  //  .header("Accept", "application/json")
+  //  .end(function (result) {
+  //    callback(result.body);
+  //  });
 });
 
 var yodaThinks = ((quote, callback) => {
@@ -55,7 +55,7 @@ const oauth = new OAuth.OAuth(
 );
 
 var dailyScheduler = ((yoda, callback) => {
-  new CronJob('0 12 13 * * *', function() {
+  new CronJob('* * * * * *', function() {
     var yodaTwit = queue.create('cheesyYoda', {
       status: `${yoda} #yodasayslove`
     })
@@ -77,6 +77,8 @@ var dailyScheduler = ((yoda, callback) => {
         });
       done();
     });
+    // supaya ga cape ganti waktu, pakai this.stop() saja, supaya dia berhenti setelah satu kali operasi
+    this.stop();
   }, null, true, 'Asia/Jakarta');
 });
 
